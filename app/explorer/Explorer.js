a18ee653c6f14915c10c9239896cb4fc44cc7b7b
maedwards <dev.edwardsm@gmail.com>
2018-06-11 14:58:37 -0400

    BE-205 Add Swagger to hyperledger explorer
    submitted on behalf of Kavya Talluri
    
    Change-Id: Iee155382fdf963edafe47ac46485412213346076
    Signed-off-by: maedwards <dev.edwardsm@gmail.com>

--- app/explorer/Explorer.js
+++ app/explorer/Explorer.js
@@ -5,13 +5,15 @@ var platformroutes = require("./rest/platformroutes");
 var explorerconfig = require("./explorerconfig.json");
 var PersistanceFactory = require("../persistance/PersistanceFactory.js");
 var timer = require("./backend/timer.js");
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 class Explorer {
     constructor() {
         this.app = express();
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
         this.persistance = {};
         this.platforms = explorerconfig["platforms"];
 
