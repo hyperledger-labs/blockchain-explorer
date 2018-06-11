f713de58da66cd425a844ac820c9e05ddf383d9a
akshay <anuguakshay@gmail.com>
2018-06-10 21:51:03 -0400

    BE-244
    REST API unit tests
    
    Change-Id: I95263ddd7cb58964df24f895578690c0418fd0ed
    Signed-off-by: akshay <anuguakshay@gmail.com>

---
+++ app/test/blockandtx.js
@@ -0,0 +1,52 @@
+
+
+var expect = require('chai').expect;
+var assert = require('assert');
+var chai = require('chai');
+var should = chai.should();
+const { spy, stub } = require('sinon');
+var config = require('../../app/platform/fabric/config')
+var appconfig = require('../../appconfig.json');
+var host = process.env.HOST || appconfig.host;
+var port = process.env.PORT || appconfig.port;
+var sinon = require('sinon');
+var request = require('request');
+const base = 'http://localhost:1337';
+const blockandtx = require('./fixtures/blockandtx.json');
+
+
+
+describe('GET /api/blockAndTxList/:channel/:blocknum', () => {
+    before(() => {
+        this.get = sinon.stub(request, 'get');
+        this.post = sinon.stub(request, 'post');
+        this.put = sinon.stub(request, 'put');
+        this.delete  = sinon.stub(request,'delete');
+    });
+
+    after(() => {
+        request.get.restore();
+        request.post.restore();
+        request.put.restore();
+        request.delete.restore();
+    });
+    it('should return blockandtx ', (done) => {
+        const obj = blockandtx;
+        this.get.yields(null, JSON.stringify(obj));
+        request.get(`${base}` + '/api/blockAndTxList/'+config['channel']+'/0', (err, body) => {
+            body = JSON.parse(body);
+            body.should.include.keys(
+                'status','rows');
+            body.status.should.eql(200);
+            for(let i=0;i<body.rows.length;i++){
+                body.rows[i].should.include.keys(
+                  'id','blocknum','datahash','txcount','channelname','prehash',
+                  'createdt','blockhash','prev_blockhash','txhash'
+
+                )
+            }
+            done();
+        });
+    });
+
+})
---
+++ app/test/chaincode.js
@@ -0,0 +1,51 @@
+
+
+var expect = require('chai').expect;
+var assert = require('assert');
+var chai = require('chai');
+var should = chai.should();
+const { spy, stub } = require('sinon');
+var config = require('../../app/platform/fabric/config')
+var appconfig = require('../../appconfig.json');
+var host = process.env.HOST || appconfig.host;
+var port = process.env.PORT || appconfig.port;
+var sinon = require('sinon');
+var request = require('request');
+const base = 'http://localhost:1337';
+const chaincode = require('./fixtures/chaincode.json');
+
+
+
+
+describe('GET /api/chaincode/:channel', () => {
+    before(() => {
+        this.get = sinon.stub(request, 'get');
+        this.post = sinon.stub(request, 'post');
+        this.put = sinon.stub(request, 'put');
+        this.delete = sinon.stub(request, 'delete');
+    });
+
+    after(() => {
+        request.get.restore();
+        request.post.restore();
+        request.put.restore();
+        request.delete.restore();
+    });
+    it('should return chaincode ', (done) => {
+        const obj = chaincode;
+        this.get.yields(null, JSON.stringify(obj));
+        request.get(`${base}` + '/api/chaincode/'+config['channel'], (err, body) => {
+            body = JSON.parse(body);
+            body.should.include.keys(
+                'status','chaincode');
+            body.status.should.eql(200);
+            for(let i=0;i<body.chaincode.length;i++){
+                body.chaincode[i].should.include.keys(
+                    'channelName','version','path',
+                    'chaincodename','txCount' )
+            }
+            done();
+        });
+    });
+
+})
