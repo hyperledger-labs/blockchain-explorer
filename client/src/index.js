7a11fef33c3e9dbd5583d5a041ca48a4da943627
jojialex2 <jojialex2@gmail.com>
2018-06-20 15:53:09 +0000

    BE-322 Carry CSS within application
    Included the css with in the application
    
    Change-Id: I7677b2074b0da0db4ca975e03f2d0706df0a3672
    Signed-off-by: jojialex2 <jojialex2@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,9 @@ import { getChannelList } from './store/actions/chanelList/action-creators';
 import { getChannel } from './store/actions/channel/action-creators';
 import './static/css/style.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'font-awesome/css/font-awesome.min.css';
 import App from './components/App/App';
 import { unregister } from './registerServiceWorker';
 
