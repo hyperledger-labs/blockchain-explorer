3dc4c97d6e264b4a48d1ff1961a906a45365c3f6
nfrunza <nfrunza@gmail.com>
2018-07-26 16:44:50 -0400

    BE-347 Permission denied error
    Change-Id: Id89114bc8ee22c4acd4d1ffe07e493576a6203a4
    Signed-off-by: nfrunza <nfrunza@gmail.com>

--- app/platform/fabric/service/chaincodeService.js
+++ app/platform/fabric/service/chaincodeService.js
@@ -3,9 +3,6 @@
 */
 var child_process = require('child_process');
 var fs = require('fs');
-// var path = "/chaincode/chaincode_example02/go/";
-//var path = "/home/maedwards/example_cc/";
-// var path = "/doesntexist/nope";
 var regXjs = '[a-z,A-Z,0-9]*.js$';
 var regXgo = '[a-z,A-Z,0-9]*.go$';
 var location;
@@ -13,22 +10,39 @@ const errors = {
   lnf: 'Location not found',
   erf: 'Error reading file'
 };
+var MAC_FIND_CMD = 'locate ';
+var CURRENT_OS = process.platform;
+let locate_cmd = 'locate -r ';
+if (CURRENT_OS === 'darwin') {
+  locate_cmd = MAC_FIND_CMD;
+}
+
 async function loadChaincodeSrc(path) {
   if (path.substring(0, 10) === 'github.com') {
     path = path.slice(10);
   }
   try {
-    location = await child_process
-      .execSync('locate -r ' + path + regXgo)
-      .toString();
+    if (CURRENT_OS === 'darwin') {
+      location = await child_process.execSync(locate_cmd).toString();
+    }
+    {
+      location = await child_process
+        .execSync(locate_cmd + path + regXgo)
+        .toString();
+    }
   } catch (error) {
     try {
-      location = await child_process
-        .execSync('locate -r ' + path + regXjs)
-        .toString();
+      if (CURRENT_OS === 'darwin') {
+        location = await child_process.execSync(locate_cmd).toString();
+      }
+      {
+        location = await child_process
+          .execSync(locate_cmd + path + regXjs)
+          .toString();
+      }
     } catch (error) {
       try {
-        location = await child_process.execSync('locate -r ' + path).toString();
+        location = await child_process.execSync(locate_cmd + path).toString();
       } catch (error) {
         location = errors.lnf;
       }
@@ -38,8 +52,34 @@ async function loadChaincodeSrc(path) {
     return errors.lnf;
   }
   var ccSource;
+  var chaincodePath;
+
   try {
-    ccSource = await child_process.execSync('cat ' + location);
+    if (Array.isArray(location) && location[0]) {
+      // get the first path
+      chaincodePath = location[0];
+    } else {
+      chaincodePath = location.split('\n');
+    }
+    if (Array.isArray(chaincodePath) && chaincodePath[0]) {
+      chaincodePath = chaincodePath[0];
+      chaincodePath = chaincodePath.trim();
+    }
+
+    var locationDirectory = chaincodePath.split('/');
+    locationDirectory = locationDirectory
+      .slice(0, locationDirectory.length - 1)
+      .join('/');
+    if (locationDirectory) {
+      fs.chmodSync(locationDirectory.trim(), '775');
+    } else {
+      return errors.lnf;
+    }
+  } catch (error) {
+    return errors.lnf;
+  }
+  try {
+    ccSource = await child_process.execSync('cat ' + chaincodePath);
   } catch (error) {
     return errors.erf;
   }
