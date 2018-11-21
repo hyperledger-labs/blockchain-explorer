6b525b388a63e276c333189a76f5c0ceae73019e
Mathilde Ffrench <mathilde.ffrench@fr.ibm.com>
2018-11-21 18:59:20 +0100

    enable db conf for simplified docker-compose

--- app/persistence/postgreSQL/db/createdb.sh
+++ app/persistence/postgreSQL/db/createdb.sh
@@ -1,32 +1,50 @@
 #!/usr/bin/env bash
 
-if [[ -z "${DATABASE_USERNAME}" ]]; then
-  USER=$( jq .pg.username pgconfig.json )
-else
-  USER=$DATABASE_USERNAME
-fi
-if [[ -z "${DATABASE_DATABASE}" ]]; then
-  DATABASE=$( jq .pg.database pgconfig.json )
-else
-  DATABASE=$DATABASE_DATABASE
-fi
-if [[ -z "${DATABASE_PASSWORD}" ]]; then
-  PASSWD=$(jq .pg.passwd pgconfig.json | sed "y/\"/'/")
-else
-  PASSWD=$DATABASE_PASSWORD
-fi
+## IF ALREADY CONFIGURED NO NEED TO RECONFIG
+if [ ! -f /${PGDATA}/conf.done ]; then
 
-echo "USER=${USER}"
-echo "DATABASE=${DATABASE}"
-echo "PASSWD=${PASSWD}"
+  if [[ -z "${DATABASE_USERNAME}" ]]; then
+    USER=$( jq .pg.username pgconfig.json )
+  else
+    USER=$DATABASE_USERNAME
+  fi
+  if [[ -z "${DATABASE_DATABASE}" ]]; then
+    DATABASE=$( jq .pg.database pgconfig.json )
+  else
+    DATABASE=$DATABASE_DATABASE
+  fi
+  if [[ -z "${DATABASE_PASSWORD}" ]]; then
+    PASSWD=$(jq .pg.passwd pgconfig.json | sed "y/\"/'/")
+  else
+    PASSWD=$DATABASE_PASSWORD
+  fi
 
-case $OSTYPE in
- darwin*) echo "Creating Default user $USER..." ;
- psql -U postgres -c "CREATE USER $USER WITH PASSWORD '$PASSWD'" ;
- psql postgres -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
- psql postgres -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql ;;
- linux*) echo "Creating Default user..." ;
- sudo -u postgres psql -U postgres -c "CREATE USER $USER WITH PASSWORD '$PASSWD'" ;
- sudo -u postgres psql -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
- sudo -u postgres psql -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql ;;
-esac
+  echo "USER=${USER}"
+  echo "DATABASE=${DATABASE}"
+  echo "PASSWD=${PASSWD}"
+
+  case $OSTYPE in
+   darwin*)
+   echo "Creating Default user $USER..." ;
+   psql -U postgres -c "CREATE USER $USER WITH PASSWORD '$PASSWD'" ;
+   psql postgres -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
+   psql postgres -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql ;;
+
+   linux*)
+   WHOAMI=`whoami`
+   if [ "${WHOAMI}" != "postgres" ]; then
+     echo "Creating Default user..." ;
+     sudo -u postgres psql -U postgres -c "CREATE USER $USER WITH PASSWORD '$PASSWD'" ;
+     sudo -u postgres psql -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
+     sudo -u postgres psql -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql
+   else
+     echo "Creating Default user..." ;
+     psql -U postgres -c "CREATE USER $USER WITH PASSWORD '$PASSWD'" ;
+     psql -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./explorerpg.sql ;
+     psql -v dbname=$DATABASE -v user=$USER -v passwd=$PASSWD -f ./updatepg.sql
+   fi
+   ;;
+  esac
+
+  touch /${PGDATA}/conf.done
+fi
