d99e1773c756f156b9b3ffef69441830611a841c
Atsushi Neki <atsushin@fast.au.fujitsu.com>
2019-10-30 07:02:19 +0000

    BE-697 Add a column for block size on BLOCK view
    The size is approximate size calculated from block data(json) given via fabric node sdk.
    
    Signed-off-by: Atsushi Neki <atsushin@fast.au.fujitsu.com>
    Change-Id: Id06b431aa01e3cee1eb0a5f598a415ad1fb2135b

--- app/platform/fabric/sync/SyncService.js
+++ app/platform/fabric/sync/SyncService.js
@@ -477,7 +477,8 @@ class SyncServices {
 				createdt,
 				prev_blockhash: '',
 				blockhash,
-				channel_genesis_hash
+				channel_genesis_hash,
+				blksize: jsonObjSize(block)
 			};
 			const txLen = block.data.data.length;
 			for (let i = 0; i < txLen; i++) {
@@ -651,7 +652,8 @@ class SyncServices {
 					} tx`,
 					time: createdt,
 					txcount: block.data.data.length,
-					datahash: block.header.data_hash
+					datahash: block.header.data_hash,
+					blksize: block_row.blksize
 				};
 
 				_self.platform.send(notify);
@@ -700,4 +702,50 @@ function convertValidationCode(code) {
 		return code;
 	}
 	return _validation_codes[code];
+}
+
+// Calculate data size of json object
+function jsonObjSize(json) {
+	let bytes = 0;
+
+	function sizeOf(obj) {
+		if (obj !== null && obj !== undefined) {
+			switch (typeof obj) {
+				case 'number': {
+					bytes += 8;
+					break;
+				}
+				case 'string': {
+					bytes += obj.length;
+					break;
+				}
+				case 'boolean': {
+					bytes += 4;
+					break;
+				}
+				case 'object': {
+					const objClass = Object.prototype.toString.call(obj).slice(8, -1);
+					if (objClass === 'Object' || objClass === 'Array') {
+						for (const key in obj) {
+							if (!Object.prototype.hasOwnProperty.call(obj, key)) continue;
+							sizeOf(obj[key]);
+						}
+					} else {
+						bytes += obj.length;
+					}
+					break;
+				}
+				default:
+					console.log(typeof obj);
+					break;
+			}
+		}
+		return bytes;
+	}
+
+	function formatByteSize(rawByte) {
+		return (rawByte / 1024).toFixed(0);
+	}
+
+	return formatByteSize(sizeOf(json));
 }
