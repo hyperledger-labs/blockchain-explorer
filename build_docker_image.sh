1e107be84c393e243ec2c46c7b7a6a285117c77d
Atsushi Neki <atsushin@fast.au.fujitsu.com>
2020-03-22 17:58:46 +1100

    BE-735 Reduce the number of node running in e2e test
    As test result is so flaky
    
    Signed-off-by: Atsushi Neki <atsushin@fast.au.fujitsu.com>

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -20,15 +20,44 @@ function banner(){
 
 function deploy_build_database(){
 	echo "Building Hyperledger Fabric Database image..."
-	docker build -f postgres-Dockerfile --tag $FABRIC_EXPLORER_DB_TAG .
+	docker build --build-arg "http_proxy=${http_proxy}" --build-arg "https_proxy=${https_proxy}" --build-arg "no_proxy=${no_proxy}" -f postgres-Dockerfile --tag $FABRIC_EXPLORER_DB_TAG .
 }
 
 function deploy_build_explorer(){
 	echo "Building Hyperledger Fabric explorer image..."
-	docker build --tag $FABRIC_EXPLORER_TAG .
+	docker build --build-arg "http_proxy=${http_proxy}" --build-arg "https_proxy=${https_proxy}" --build-arg "no_proxy=${no_proxy}" --tag $FABRIC_EXPLORER_TAG .
 }
 
 banner
 
-deploy_build_explorer
-deploy_build_database
+build_db=0
+build_explorer=0
+build_all=1
+
+while getopts "de" opt; do
+  case "$opt" in
+  d)
+    echo "Build DB images"
+    build_db=1
+		build_all=0
+    ;;
+  e)
+    echo "Build Explorer images"
+    build_explorer=1
+		build_all=0
+    ;;
+  esac
+done
+
+if [ $build_all -eq 1 ]; then
+	deploy_build_explorer
+	deploy_build_database
+else
+	if [ $build_db -eq 1 ]; then
+		deploy_build_database
+	fi
+
+	if [ $build_explorer -eq 1 ]; then
+		deploy_build_explorer
+	fi
+fi
