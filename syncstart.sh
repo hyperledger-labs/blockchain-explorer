4201d71f7b5a859e703cb9ec0b78ac19d8fc004e
Atsushi Neki <nekiaiken@gmail.com>
2020-12-07 11:48:05 +1100

    BE-834 Fix memory leak issue in the setup of DS
    Changed to keep a discovery service instance after initializing it at
    the boot phase.
    
    Signed-off-by: Atsushi Neki <nekiaiken@gmail.com>

--- syncstart.sh
+++ syncstart.sh
@@ -7,22 +7,18 @@
 # Please visit ./logs/sync/app to view the application logs and visit the ./logs/sync/db to view the Database logs and visit the ./logs/sync/console for the console.log
 # Log rotating for every 7 days.
 
-rm -rf /tmp/fabric-client-kvs_peerOrg*
-
 export SYNC_LOG_PATH=./logs/sync
 
 echo "************************************************************************************"
 echo "**************************** Hyperledger Explorer Sync**********************************"
 echo "************************************************************************************"
 
-export LOG_LEVEL_APP=debug
-export LOG_LEVEL_DB=debug
-export LOG_LEVEL_CONSOLE=info
-export LOG_CONSOLE_STDOUT=false
+export LOG_LEVEL_APP=${LOG_LEVEL_APP:-debug}
+export LOG_LEVEL_DB=${LOG_LEVEL_DB:-debug}
+export LOG_LEVEL_CONSOLE=${LOG_LEVEL_CONSOLE:-info}
+export LOG_CONSOLE_STDOUT=${LOG_CONSOLE_STDOUT:-false}
 
-export DISCOVERY_AS_LOCALHOST=true
-node sync.js $1 $2 &
+export DISCOVERY_AS_LOCALHOST=${DISCOVERY_AS_LOCALHOST:-true}
+export EXPLORER_APP_ROOT=${EXPLORER_APP_ROOT:-dist}
 
-
-
-
+node ${EXPLORER_APP_ROOT}/sync.js $1 $2 &
