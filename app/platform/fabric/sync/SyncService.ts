d9f846d3b348d4c4448df225abf801346dc44ae3
Atsushin <atsushin@fast.au.fujitsu.com>
2021-01-26 18:15:54 +1100

    BE-852 Fix to get RWset for config TX synced correctly (#211)
    * BE-852 Fix to get RWset for config TX synced correctly
    
    Also added computation of ID for config TX
    
    Signed-off-by: Atsushi Neki <nekiaiken@gmail.com>
    
    * BE-852 Add unit test for SyncServices class
    
    Mainly added test cases for processBlockEvent method.
    
    Signed-off-by: Atsushi Neki <nekiaiken@gmail.com>

--- app/platform/fabric/sync/SyncService.ts
+++ app/platform/fabric/sync/SyncService.ts
@@ -5,6 +5,7 @@
 import * as convertHex from 'convert-hex';
 import fabprotos from 'fabric-protos';
 import includes from 'lodash/includes';
+import * as sha from 'js-sha256';
 import { helper } from '../../../common/helper';
 
 import { ExplorerError } from '../../../common/ExplorerError';
@@ -15,8 +16,6 @@ import * as FabricUtils from '../../../platform/fabric/utils/FabricUtils';
 const logger = helper.getLogger('SyncServices');
 
 const fabric_const = FabricConst.fabric.const;
-
-const blocksInProcess = [];
 
 // Transaction validation code
 const _validation_codes = {};
@@ -34,6 +33,7 @@ export class SyncServices {
 	persistence: any;
 	platform: any;
 	synchInProcess: string[];
+	blocksInProcess: string[];
 
 	/**
 	 * Creates an instance of SyncServices.
@@ -45,6 +45,7 @@ export class SyncServices {
 		this.platform = platform;
 		this.persistence = persistence;
 		this.synchInProcess = [];
+		this.blocksInProcess = [];
 	}
 
 	/**
@@ -390,6 +391,50 @@ export class SyncServices {
 		this.synchInProcess.splice(index, 1);
 	}
 
+	async updateDiscoveredChannel(client, channel_name, channel_genesis_hash) {
+		const network_id = client.getNetworkId();
+		// get discovery and insert new peer, orders details to db
+		await client.initializeChannelFromDiscover(channel_name);
+		await this.insertFromDiscoveryResults(
+			client,
+			channel_name,
+			channel_genesis_hash
+		);
+		const notify = {
+			notify_type: fabric_const.NOTITY_TYPE_UPDATECHANNEL,
+			network_id,
+			channel_name
+		};
+
+		this.platform.send(notify);
+	}
+
+	async insertDiscoveredChannel(client, channel_name, block) {
+		const network_id = client.getNetworkId();
+		await client.initializeNewChannel(channel_name);
+		const channel_genesis_hash = client.getChannelGenHash(channel_name);
+		// inserting new channel details to DB
+		await this.insertNewChannel(
+			client,
+			channel_name,
+			block,
+			channel_genesis_hash
+		);
+		await this.insertFromDiscoveryResults(
+			client,
+			channel_name,
+			channel_genesis_hash
+		);
+
+		const notify = {
+			notify_type: fabric_const.NOTITY_TYPE_NEWCHANNEL,
+			network_id,
+			channel_name
+		};
+
+		this.platform.send(notify);
+	}
+
 	/**
 	 *
 	 *
@@ -407,53 +452,34 @@ export class SyncServices {
 		const channel_name = header.channel_header.channel_id;
 		const blockPro_key = `${channel_name}_${block.header.number.toString()}`;
 
-		if (blocksInProcess.includes(blockPro_key)) {
-			return 'Block already in processing';
-		}
-		blocksInProcess.push(blockPro_key);
-
 		logger.debug('New Block  >>>>>>> %j', block.header.number);
-		let channel_genesis_hash = client.getChannelGenHash(channel_name);
+		const channel_genesis_hash = client.getChannelGenHash(channel_name);
 		// Checking block is channel CONFIG block
 		/* eslint-disable */
 		if (!channel_genesis_hash) {
 			// get discovery and insert channel details to db and create new channel object in client context
 			setTimeout(
 				async (cli, chName, blk) => {
-					await cli.initializeNewChannel(chName);
-					channel_genesis_hash = cli.getChannelGenHash(chName);
-					// inserting new channel details to DB
-					await this.insertNewChannel(cli, chName, blk, channel_genesis_hash);
-					await this.insertFromDiscoveryResults(cli, chName, channel_genesis_hash);
-
-					const notify = {
-						notify_type: fabric_const.NOTITY_TYPE_NEWCHANNEL,
-						network_id,
-						channel_name: chName
-					};
-
-					this.platform.send(notify);
+					await this.insertDiscoveredChannel(cli, chName, blk);
 				},
 				10000,
 				client,
 				channel_name,
 				block
 			);
-		} else if (
-			header.channel_header.typeString === fabric_const.BLOCK_TYPE_CONFIG
-		) {
+			logger.warn('Insert discovered new channel', channel_name);
+			throw new ExplorerError(`${channel_name} has not been inserted yet`);
+		}
+
+		if (this.blocksInProcess.includes(blockPro_key)) {
+			throw new ExplorerError('Block already in processing');
+		}
+		this.blocksInProcess.push(blockPro_key);
+
+		if (header.channel_header.typeString === fabric_const.BLOCK_TYPE_CONFIG) {
 			setTimeout(
 				async (cli, chName, chGenHash) => {
-					// get discovery and insert new peer, orders details to db
-					await cli.initializeChannelFromDiscover(chName);
-					await this.insertFromDiscoveryResults(cli, chName, chGenHash);
-					const notify = {
-						notify_type: fabric_const.NOTITY_TYPE_UPDATECHANNEL,
-						network_id,
-						channel_name: chName
-					};
-
-					this.platform.send(notify);
+					await this.updateDiscoveredChannel(cli, chName, chGenHash);
 				},
 				10000,
 				client,
@@ -461,208 +487,218 @@ export class SyncServices {
 				channel_genesis_hash
 			);
 		}
-		/* eslint-enable */
+
 		const createdt = await FabricUtils.getBlockTimeStamp(
 			header.channel_header.timestamp
 		);
 		const blockhash = await FabricUtils.generateBlockHash(block.header);
-		if (channel_genesis_hash) {
-			const block_row = {
-				blocknum: block.header.number.toString(),
-				datahash: block.header.data_hash.toString('hex'),
-				prehash: block.header.previous_hash.toString('hex'),
-				txcount: block.data.data.length,
-				createdt,
-				prev_blockhash: '',
-				blockhash,
-				channel_genesis_hash,
-				blksize: jsonObjSize(block)
-			};
-			const txLen = block.data.data.length;
-			for (let txIndex = 0; txIndex < txLen; txIndex++) {
-				const txObj = block.data.data[txIndex];
-				const txid = txObj.payload.header.channel_header.tx_id;
-				let validation_code = '';
-				let endorser_signature = '';
-				let payload_proposal_hash = '';
-				let endorser_id_bytes = '';
-				let chaincode_proposal_input = '';
-				let chaincode = '';
-				let rwset;
-				let readSet;
-				let writeSet;
-				let chaincodeID;
-				let status;
-				let mspId = [];
-				this.convertFormatOfValue(
-					'value',
-					client.fabricGateway.fabricConfig.getRWSetEncoding(),
-					txObj
+		const block_row = {
+			blocknum: block.header.number.toString(),
+			datahash: block.header.data_hash.toString('hex'),
+			prehash: block.header.previous_hash.toString('hex'),
+			txcount: block.data.data.length,
+			createdt,
+			prev_blockhash: '',
+			blockhash,
+			channel_genesis_hash,
+			blksize: jsonObjSize(block)
+		};
+		const txLen = block.data.data.length;
+		for (let txIndex = 0; txIndex < txLen; txIndex++) {
+			const txObj = block.data.data[txIndex];
+			const txStr = JSON.stringify(txObj);
+			const size = Buffer.byteLength(txStr);
+			let txid = txObj.payload.header.channel_header.tx_id;
+
+			let validation_code = '';
+			let endorser_signature = '';
+			let payload_proposal_hash = '';
+			let endorser_id_bytes = '';
+			let chaincode_proposal_input = '';
+			let chaincode = '';
+			let rwset;
+			let readSet;
+			let writeSet;
+			let chaincodeID;
+			let status;
+			let mspId = [];
+
+			this.convertFormatOfValue(
+				'value',
+				client.fabricGateway.fabricConfig.getRWSetEncoding(),
+				txObj
+			);
+			if (txid && txid !== '') {
+				const validation_codes =
+					block.metadata.metadata[
+						fabprotos.common.BlockMetadataIndex.TRANSACTIONS_FILTER
+					];
+				const val_code = validation_codes[txIndex];
+				validation_code = convertValidationCode(val_code);
+			}
+			let envelope_signature = txObj.signature;
+			if (envelope_signature !== undefined) {
+				envelope_signature = convertHex.bytesToHex(envelope_signature);
+			}
+			let payload_extension = txObj.payload.header.channel_header.extension;
+			if (payload_extension !== undefined) {
+				payload_extension = convertHex.bytesToHex(payload_extension);
+			}
+			let creator_nonce = txObj.payload.header.signature_header.nonce;
+			if (creator_nonce !== undefined) {
+				creator_nonce = convertHex.bytesToHex(creator_nonce);
+			}
+			/* eslint-disable */
+			const creator_id_bytes =
+				txObj.payload.header.signature_header.creator.id_bytes;
+			if (txObj.payload.data.actions !== undefined) {
+				chaincode =
+					txObj.payload.data.actions[0].payload.action.proposal_response_payload
+						.extension.chaincode_id.name;
+				chaincodeID = new Uint8Array(
+					txObj.payload.data.actions[0].payload.action.proposal_response_payload.extension
 				);
-				if (txid && txid !== '') {
-					const validation_codes =
-						block.metadata.metadata[
-							fabprotos.common.BlockMetadataIndex.TRANSACTIONS_FILTER
-						];
-					const val_code = validation_codes[txIndex];
-					validation_code = convertValidationCode(val_code);
-				}
-				let envelope_signature = txObj.signature;
-				if (envelope_signature !== undefined) {
-					envelope_signature = convertHex.bytesToHex(envelope_signature);
-				}
-				let payload_extension = txObj.payload.header.channel_header.extension;
-				if (payload_extension !== undefined) {
-					payload_extension = convertHex.bytesToHex(payload_extension);
-				}
-				let creator_nonce = txObj.payload.header.signature_header.nonce;
-				if (creator_nonce !== undefined) {
-					creator_nonce = convertHex.bytesToHex(creator_nonce);
-				}
-				/* eslint-disable */
-				const creator_id_bytes =
-					txObj.payload.header.signature_header.creator.id_bytes;
-				if (txObj.payload.data.actions !== undefined) {
-					chaincode =
-						txObj.payload.data.actions[0].payload.action.proposal_response_payload
-							.extension.chaincode_id.name;
-					chaincodeID = new Uint8Array(
-						txObj.payload.data.actions[0].payload.action.proposal_response_payload.extension
-					);
-					status =
-						txObj.payload.data.actions[0].payload.action.proposal_response_payload
-							.extension.response.status;
-					mspId = txObj.payload.data.actions[0].payload.action.endorsements.map(
-						endorsement => endorsement.endorser.mspid
-					);
-					rwset =
-						txObj.payload.data.actions[0].payload.action.proposal_response_payload
-							.extension.results.ns_rwset;
-					readSet = rwset.map(rw => ({
-						chaincode: rw.namespace,
-						set: rw.rwset.reads
-					}));
-					writeSet = rwset.map(rw => ({
-						chaincode: rw.namespace,
-						set: rw.rwset.writes
-					}));
-					chaincode_proposal_input =
-						txObj.payload.data.actions[0].payload.chaincode_proposal_payload.input
-							.chaincode_spec.input.args;
-					if (chaincode_proposal_input !== undefined) {
-						let inputs = '';
-						for (const input of chaincode_proposal_input) {
-							inputs =
-								(inputs === '' ? inputs : `${inputs},`) + convertHex.bytesToHex(input);
-						}
-						chaincode_proposal_input = inputs;
+				status =
+					txObj.payload.data.actions[0].payload.action.proposal_response_payload
+						.extension.response.status;
+				mspId = txObj.payload.data.actions[0].payload.action.endorsements.map(
+					endorsement => endorsement.endorser.mspid
+				);
+				rwset =
+					txObj.payload.data.actions[0].payload.action.proposal_response_payload
+						.extension.results.ns_rwset;
+				readSet = rwset.map(rw => ({
+					chaincode: rw.namespace,
+					set: rw.rwset.reads
+				}));
+				writeSet = rwset.map(rw => ({
+					chaincode: rw.namespace,
+					set: rw.rwset.writes
+				}));
+				chaincode_proposal_input =
+					txObj.payload.data.actions[0].payload.chaincode_proposal_payload.input
+						.chaincode_spec.input.args;
+				if (chaincode_proposal_input !== undefined) {
+					let inputs = '';
+					for (const input of chaincode_proposal_input) {
+						inputs =
+							(inputs === '' ? inputs : `${inputs},`) + convertHex.bytesToHex(input);
 					}
-					endorser_signature =
-						txObj.payload.data.actions[0].payload.action.endorsements[0].signature;
-					if (endorser_signature !== undefined) {
-						endorser_signature = convertHex.bytesToHex(endorser_signature);
-					}
-					payload_proposal_hash = txObj.payload.data.actions[0].payload.action.proposal_response_payload.proposal_hash.toString(
-						'hex'
-					);
-					endorser_id_bytes =
-						txObj.payload.data.actions[0].payload.action.endorsements[0].endorser
-							.IdBytes;
+					chaincode_proposal_input = inputs;
 				}
-
-				const read_set = JSON.stringify(readSet, null, 2);
-				const write_set = JSON.stringify(writeSet, null, 2);
-
-				const chaincode_id = String.fromCharCode.apply(null, chaincodeID);
-				// checking new chaincode is deployed
-				if (
-					header.channel_header.typeString ===
-						fabric_const.BLOCK_TYPE_ENDORSER_TRANSACTION &&
-					chaincode === fabric_const.CHAINCODE_LSCC
-				) {
-					setTimeout(
-						async (cli, chName, chGenHash) => {
-							// get discovery and insert chaincode details to db
-							await this.insertFromDiscoveryResults(cli, chName, chGenHash);
-
-							const notify = {
-								notify_type: fabric_const.NOTITY_TYPE_CHAINCODE,
-								network_id,
-								channel_name: chName
-							};
-
-							this.platform.send(notify);
-						},
-						10000,
-						client,
-						channel_name,
-						channel_genesis_hash
-					);
+				endorser_signature =
+					txObj.payload.data.actions[0].payload.action.endorsements[0].signature;
+				if (endorser_signature !== undefined) {
+					endorser_signature = convertHex.bytesToHex(endorser_signature);
 				}
-				/* eslint-enable */
-				const transaction_row = {
-					blockid: block.header.number.toString(),
-					txhash: txObj.payload.header.channel_header.tx_id,
-					createdt: txObj.payload.header.channel_header.timestamp,
-					chaincodename: chaincode,
-					chaincode_id,
-					status,
-					creator_msp_id: txObj.payload.header.signature_header.creator.mspid,
-					endorser_msp_id: mspId,
-					type: txObj.payload.header.channel_header.typeString,
-					read_set,
-					write_set,
-					channel_genesis_hash,
-					validation_code,
-					envelope_signature,
-					payload_extension,
-					creator_nonce,
-					chaincode_proposal_input,
-					endorser_signature,
-					creator_id_bytes,
-					payload_proposal_hash,
-					endorser_id_bytes
-				};
-
-				// Insert transaction
-
-				const res = await this.persistence
-					.getCrudService()
-					.saveTransaction(network_id, transaction_row);
-				logger.debug('saveTransaction ', res);
+				payload_proposal_hash = txObj.payload.data.actions[0].payload.action.proposal_response_payload.proposal_hash.toString(
+					'hex'
+				);
+				endorser_id_bytes =
+					txObj.payload.data.actions[0].payload.action.endorsements[0].endorser
+						.IdBytes;
 			}
 
-			// Insert block
-			logger.info('block_row.blocknum ', block_row.blocknum);
-			const successSaveBlock = await this.persistence
-				.getCrudService()
-				.saveBlock(network_id, block_row);
-			logger.debug('result of SaveBlock ', successSaveBlock);
+			if (txObj.payload.header.channel_header.typeString === 'CONFIG') {
+				txid = sha.sha256(txStr);
+				readSet =
+					txObj.payload.data.last_update.payload.data.config_update.read_set;
+				writeSet =
+					txObj.payload.data.last_update.payload.data.config_update.write_set;
+			}
 
-			if (successSaveBlock) {
-				// Push last block
-				const notify = {
-					notify_type: fabric_const.NOTITY_TYPE_BLOCK,
-					network_id,
+			const read_set = JSON.stringify(readSet, null, 2);
+			const write_set = JSON.stringify(writeSet, null, 2);
+
+			const chaincode_id = String.fromCharCode.apply(null, chaincodeID);
+			// checking new chaincode is deployed
+			if (
+				header.channel_header.typeString ===
+					fabric_const.BLOCK_TYPE_ENDORSER_TRANSACTION &&
+				chaincode === fabric_const.CHAINCODE_LSCC
+			) {
+				setTimeout(
+					async (cli, chName, chGenHash) => {
+						// get discovery and insert chaincode details to db
+						await this.insertFromDiscoveryResults(cli, chName, chGenHash);
+
+						const notify = {
+							notify_type: fabric_const.NOTITY_TYPE_CHAINCODE,
+							network_id,
+							channel_name: chName
+						};
+
+						this.platform.send(notify);
+					},
+					10000,
+					client,
 					channel_name,
-					title: `Block ${block.header.number.toString()} added to Channel: ${channel_name}`,
-					type: 'block',
-					message: `Block ${block.header.number.toString()} established with ${
-						block.data.data.length
-					} tx`,
-					time: createdt,
-					txcount: block.data.data.length,
-					datahash: block.header.data_hash.toString('hex'),
-					blksize: block_row.blksize
-				};
-
-				this.platform.send(notify);
+					channel_genesis_hash
+				);
 			}
-		} else {
-			logger.error('Failed to process the block %j', block);
+			/* eslint-enable */
+			const transaction_row = {
+				blockid: block.header.number.toString(),
+				txhash: txid,
+				createdt: txObj.payload.header.channel_header.timestamp,
+				chaincodename: chaincode,
+				chaincode_id,
+				status,
+				creator_msp_id: txObj.payload.header.signature_header.creator.mspid,
+				endorser_msp_id: mspId,
+				type: txObj.payload.header.channel_header.typeString,
+				read_set,
+				write_set,
+				channel_genesis_hash,
+				validation_code,
+				envelope_signature,
+				payload_extension,
+				creator_nonce,
+				chaincode_proposal_input,
+				endorser_signature,
+				creator_id_bytes,
+				payload_proposal_hash,
+				endorser_id_bytes
+			};
+
+			// Insert transaction
+
+			const res = await this.persistence
+				.getCrudService()
+				.saveTransaction(network_id, transaction_row);
+			logger.debug('saveTransaction ', res);
 		}
-		const index = blocksInProcess.indexOf(blockPro_key);
-		blocksInProcess.splice(index, 1);
+
+		// Insert block
+		logger.info('block_row.blocknum ', block_row.blocknum);
+		const successSaveBlock = await this.persistence
+			.getCrudService()
+			.saveBlock(network_id, block_row);
+		logger.debug('result of SaveBlock ', successSaveBlock);
+
+		if (successSaveBlock) {
+			// Push last block
+			const notify = {
+				notify_type: fabric_const.NOTITY_TYPE_BLOCK,
+				network_id,
+				channel_name,
+				title: `Block ${block.header.number.toString()} added to Channel: ${channel_name}`,
+				type: 'block',
+				message: `Block ${block.header.number.toString()} established with ${
+					block.data.data.length
+				} tx`,
+				time: createdt,
+				txcount: block.data.data.length,
+				datahash: block.header.data_hash.toString('hex'),
+				blksize: block_row.blksize
+			};
+
+			this.platform.send(notify);
+		}
+
+		const index = this.blocksInProcess.indexOf(blockPro_key);
+		this.blocksInProcess.splice(index, 1);
+		return true;
 	}
 
 	convertFormatOfValue(prop, encoding, obj) {
---
+++ app/test/SyncService.test.ts
@@ -0,0 +1,179 @@
+/*
+ *SPDX-License-Identifier: Apache-2.0
+ */
+
+import { expect } from './expect';
+import { FabricConfig } from '../platform/fabric/FabricConfig';
+
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+import { SyncServices } from '../platform/fabric/sync/SyncService';
+import * as stubBlock from './block.json';
+import * as stubConfigBlock from './block_config.json';
+
+import { ExplorerError } from '../common/ExplorerError';
+import * as FabricConst from '../platform/fabric/utils/FabricConst';
+const fabric_const = FabricConst.fabric.const;
+
+// logger
+const stubError = sinon.stub();
+const stubInfo = sinon.stub();
+const stubDebug = sinon.stub();
+
+const VALID_GENESIS_HASH = '8A+HyzS4sqZynD06BfNW7T1Vtv2SOXAOUJQK4itulus=';
+const VALID_NETWORK_ID = 'test-network-id';
+
+const stubPlatform = {
+	send: sinon.spy()
+};
+const spySaveTransaction = sinon.spy();
+const stubGetChannelGenHash = sinon.stub();
+
+function getSyncServicesInstance() {
+	const { SyncServices } = proxyquire
+		.noCallThru()
+		.load('../platform/fabric/sync/SyncService', {
+			'convert-hex': {
+				bytesToHex: sinon.stub()
+			},
+			'../../../common/helper': {
+				helper: {
+					getLogger: function() {
+						return {
+							error: stubError,
+							info: stubInfo,
+							debug: stubDebug,
+							warn: sinon.stub()
+						};
+					}
+				}
+			}
+		});
+	const config = new FabricConfig();
+	config.initialize('first-network', {
+		name: 'My first network',
+		profile: './connection-profile/first-network.json'
+	});
+	sinon.stub(config, 'getPeerTlsCACertsPem');
+	const stubGetCrudService = sinon.stub();
+	stubGetCrudService.returns({
+		saveBlock: sinon.stub().resolves(true),
+		saveTransaction: spySaveTransaction
+	});
+	const stubPersistence = {
+		getCrudService: stubGetCrudService
+	};
+	const sync = new SyncServices(stubPlatform, stubPersistence);
+
+	return sync;
+}
+
+function resetAllStubs(sync) {
+	// logger
+	stubError.reset();
+	stubInfo.reset();
+
+	sync.blocksInProcess = [];
+	stubGetChannelGenHash.reset();
+	stubPlatform.send.resetHistory();
+	spySaveTransaction.resetHistory();
+}
+
+function setupClient() {
+	const stubGetNetworkID = sinon.stub();
+	stubGetNetworkID.returns(VALID_NETWORK_ID);
+	stubGetChannelGenHash.returns(VALID_GENESIS_HASH);
+	const stubClient = {
+		getNetworkId: stubGetNetworkID,
+		getChannelGenHash: stubGetChannelGenHash,
+		initializeNewChannel: sinon.stub().resolves(true),
+		fabricGateway: {
+			fabricConfig: {
+				getRWSetEncoding: sinon.stub()
+			}
+		}
+	};
+	return stubClient;
+}
+
+describe('processBlockEvent', () => {
+	let sync: SyncServices;
+
+	before(() => {
+		sync = getSyncServicesInstance();
+	});
+
+	beforeEach(() => {
+		resetAllStubs(sync);
+	});
+
+	it('should return without error', async () => {
+		const stubClient = setupClient();
+
+		await expect(sync.processBlockEvent(stubClient, stubBlock)).eventually.to.be
+			.true;
+		sinon.assert.calledOnce(stubPlatform.send);
+		sinon.assert.calledWith(
+			stubPlatform.send,
+			sinon.match({ notify_type: fabric_const.NOTITY_TYPE_BLOCK })
+		);
+		expect(sync.blocksInProcess.length).equals(0);
+	});
+
+	it('should throw an error if it has already been in proces', async () => {
+		const stubClient = setupClient();
+		sync.blocksInProcess = ['mychannel_9'];
+
+		await expect(sync.processBlockEvent(stubClient, stubBlock))
+			.to.eventually.be.rejectedWith('Block already in processing')
+			.and.be.an.instanceOf(ExplorerError);
+		sinon.assert.notCalled(stubPlatform.send);
+		expect(sync.blocksInProcess.length).equals(1);
+	});
+
+	it('should raise new channel notification if genesis has not already been stored yet', async () => {
+		const stubClient = setupClient();
+
+		stubGetChannelGenHash.onFirstCall().returns(null);
+		stubGetChannelGenHash.onSecondCall().returns(VALID_GENESIS_HASH);
+		stubClient.getChannelGenHash = stubGetChannelGenHash;
+		const spyInsertDiscoveredCH = sinon.spy(sync, 'insertDiscoveredChannel');
+
+		const clock = sinon.useFakeTimers();
+		await expect(sync.processBlockEvent(stubClient, stubBlock))
+			.to.eventually.be.rejectedWith('mychannel has not been inserted yet')
+			.and.be.an.instanceOf(ExplorerError);
+		clock.tick(20000);
+
+		sinon.assert.calledOnce(spyInsertDiscoveredCH);
+		expect(sync.blocksInProcess.length).equals(0);
+		clock.restore();
+	});
+
+	it('should raise update channel notification if config block is processed', async () => {
+		const stubClient = setupClient();
+
+		const spyUpdateDiscoveredCH = sinon.spy(sync, 'updateDiscoveredChannel');
+
+		const clock = sinon.useFakeTimers();
+		await expect(sync.processBlockEvent(stubClient, stubConfigBlock)).to
+			.eventually.to.be.true;
+		clock.tick(20000);
+
+		sinon.assert.calledWith(
+			spySaveTransaction,
+			VALID_NETWORK_ID,
+			sinon.match(obj => {
+				return 'txhash' in obj && obj['txhash'] !== undefined;
+			}, 'txhash undefined')
+		);
+		sinon.assert.calledOnce(spyUpdateDiscoveredCH);
+		expect(sync.blocksInProcess.length).equals(0);
+		sinon.assert.calledOnce(stubPlatform.send);
+		sinon.assert.calledWith(
+			stubPlatform.send,
+			sinon.match({ notify_type: fabric_const.NOTITY_TYPE_BLOCK })
+		);
+		clock.restore();
+	});
+});
