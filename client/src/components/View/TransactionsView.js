b28b16eff385197d581be9f7f60e75dcef31a856
anilspecial <35342828+anilspecial@users.noreply.github.com>
2021-04-08 09:52:38 -0500

    Be 694 UI link to trans (#227)
    * Bump react-dev-utils from 11.0.3 to 11.0.4 in /client (#222)
    
    Bumps [react-dev-utils](https://github.com/facebook/create-react-app/tree/HEAD/packages/react-dev-utils) from 11.0.3 to 11.0.4.
    - [Release notes](https://github.com/facebook/create-react-app/releases)
    - [Changelog](https://github.com/facebook/create-react-app/blob/master/CHANGELOG-1.x.md)
    - [Commits](https://github.com/facebook/create-react-app/commits/HEAD/packages/react-dev-utils)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Signed-off-by: Anil Kumar <anil.special2018@gmail.com>
    
    * new file repolint-json (#223) (#3)
    
    Signed-off-by: Anil Kumar <anil.special2018@gmail.com>
    
    * direct link to a transaction
    
    Signed-off-by: Anil Kumar <anil.special2018@gmail.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- client/src/components/View/TransactionsView.js
+++ client/src/components/View/TransactionsView.js
@@ -6,47 +6,51 @@ import React from 'react';
 import View from '../Styled/View';
 import Transactions from '../Lists/Transactions';
 import {
-  currentChannelType,
-  getTransactionType,
-  getTransactionInfoType,
-  getTransactionListType,
-  transactionType,
-  transactionListType,
+	currentChannelType,
+	getTransactionType,
+	getTransactionInfoType,
+	getTransactionListType,
+	transactionType,
+	transactionListType
 } from '../types';
 
 export const TransactionsView = ({
-  currentChannel,
-  getTransaction,
-  getTransactionInfo,
-  getTransactionList,
-  transaction,
-  transactionList,
-  getTransactionListSearch,
-  transactionByOrg,
-  transactionListSearch,
+	currentChannel,
+	getTransaction,
+	getTransactionInfo,
+	getTransactionList,
+	transaction,
+	transactionList,
+	getTransactionListSearch,
+	transactionByOrg,
+	transactionListSearch,
+	transactionId,
+	removeTransactionId
 }) => (
-  <View>
-    <Transactions
-      currentChannel={currentChannel}
-      transactionList={transactionList}
-      getTransactionList={getTransactionList}
-      transaction={transaction}
-      transactionByOrg={transactionByOrg}
-      getTransactionInfo={getTransactionInfo}
-      getTransaction={getTransaction}
-      getTransactionListSearch={getTransactionListSearch}
-      transactionListSearch={transactionListSearch}
-    />
-  </View>
+	<View>
+		<Transactions
+			currentChannel={currentChannel}
+			transactionList={transactionList}
+			getTransactionList={getTransactionList}
+			transaction={transaction}
+			transactionByOrg={transactionByOrg}
+			getTransactionInfo={getTransactionInfo}
+			getTransaction={getTransaction}
+			getTransactionListSearch={getTransactionListSearch}
+			transactionListSearch={transactionListSearch}
+			transactionId={transactionId}
+			removeTransactionId={removeTransactionId}
+		/>
+	</View>
 );
 
 TransactionsView.propTypes = {
-  currentChannel: currentChannelType.isRequired,
-  getTransaction: getTransactionType.isRequired,
-  getTransactionInfo: getTransactionInfoType.isRequired,
-  getTransactionList: getTransactionListType.isRequired,
-  transaction: transactionType.isRequired,
-  transactionList: transactionListType.isRequired,
+	currentChannel: currentChannelType.isRequired,
+	getTransaction: getTransactionType.isRequired,
+	getTransactionInfo: getTransactionInfoType.isRequired,
+	getTransactionList: getTransactionListType.isRequired,
+	transaction: transactionType.isRequired,
+	transactionList: transactionListType.isRequired
 };
 
 export default TransactionsView;
