9e0a3235bcd57a00bd7266ddea4065b236d7fc06
Kaushik Kumar Bora <kaushikkumarbora@gmail.com>
2022-05-12 20:15:43 +0530

    Fix ill-formed response by /api/peersStatus (#297)
    * Fix ill-formed response by /api/peersStatus
    
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>
    
    * linted
    
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>

--- app/platform/fabric/Proxy.ts
+++ app/platform/fabric/Proxy.ts
@@ -137,12 +137,14 @@ export class Proxy {
 							node.ledger_height_unsigned = peer.ledgerHeight.unsigned;
 						}
 					}
-				} else {
-					// Sometime 'peers_by_org' property is not included in discover result
-					node.ledger_height_low = '-';
-					node.ledger_height_high = '-';
-					node.ledger_height_unsigned = '-';
 				}
+				// Sometime 'peers_by_org' property is not included in discover result
+				if(typeof node.ledger_height_low === 'undefined')
+					node.ledger_height_low = '-';
+				if(typeof node.ledger_height_high === 'undefined')
+					node.ledger_height_high = '-';
+				if(typeof node.ledger_height_unsigned === 'undefined')
+					node.ledger_height_unsigned = '-';
 				peers.push(node);
 			} else if (node.peer_type === 'ORDERER') {
 				node.status = 'DOWN';
