92a2dcfc9de00220c29e433f375854ac507e1279
ChangHyunKim <changhyun.kim@ssvt.co.kr>
2022-11-08 09:50:21 +0900

    Add core redux state for drawer

--- client/src/state/redux/index.js
+++ client/src/state/redux/index.js
@@ -6,8 +6,10 @@ import { default as auth } from './auth';
 import { default as charts } from './charts';
 import { default as tables } from './tables';
 import { default as theme } from './theme';
+import { default as core } from './core';
 
 export { auth };
 export { charts };
 export { tables };
 export { theme };
+export { core };
