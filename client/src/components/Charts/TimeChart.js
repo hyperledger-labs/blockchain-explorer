f03d105bbff04af5f46d15a2786048b08e36e3f4
ChangHyunKim <changhyun.kim@ssvt.co.kr>
2022-11-09 15:45:26 +0900

    Change TimeChart styles

--- client/src/components/Charts/TimeChart.js
+++ client/src/components/Charts/TimeChart.js
@@ -3,67 +3,65 @@
  */
 
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import {
-  ResponsiveContainer,
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
+	ResponsiveContainer,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	AreaChart,
+	Area
 } from 'recharts';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import { chartDataType } from '../types';
 
-const styles = (theme) => {
-  const { type } = theme.palette;
-  const dark = type === 'dark';
-  return {
-    content: {
-      backgroundColor: dark ? '#3c3558' : undefined,
-      '& .recharts-layer': {
-        fill: dark ? 'rgb(42, 173, 230) !important' : '#5bc5c2 !important',
-      },
-      '& .recharts-scatter-line': {
-        stroke: dark ? '#ffc145 !important' : '#5bc5c2 !important',
-        strokeWidth: '2 !important',
-      },
-      '& .recharts-text': {
-        fill: dark ? '#ffffff !important' : undefined,
-      },
-      '& .recharts-cartesian-axis-line': {
-        stroke: dark ? '#ffffff' : undefined,
-      },
-    },
-  };
-};
-
-export const TimeChart = ({ chartData, classes }) => (
-  <div>
-    <Card>
-      <CardContent className={classes.content}>
-        <ResponsiveContainer width="100%" height={255}>
-          <ScatterChart>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="datetime" className="datetime" />
-            <YAxis domain={[0, chartData.dataMax]} dataKey="count" />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter
-              className="datetime"
-              data={chartData.displayData}
-              line={{}}
-            />
-          </ScatterChart>
-        </ResponsiveContainer>
-      </CardContent>
-    </Card>
-  </div>
+export const TimeChart = ({ chartData }) => (
+	<ResponsiveContainer width="100%" height={225}>
+		<AreaChart data={chartData.displayData}>
+			<defs>
+				<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+					<stop offset="10%" stopColor="#D4E4FE" stopOpacity={1} />
+					<stop offset="100%" stopColor="#D4E4FE" stopOpacity={0} />
+				</linearGradient>
+			</defs>
+			<CartesianGrid strokeDasharray="3" vertical={false} />
+			<XAxis
+				dataKey="datetime"
+				className="datetime"
+				tickLine={false}
+				axisLine={false}
+				tickCount={5}
+			/>
+			<YAxis
+				domain={[0, chartData.dataMax]}
+				dataKey="count"
+				tickLine={false}
+				axisLine={false}
+			/>
+			<Tooltip
+				cursor={{
+					stroke: 'rgba(0, 0, 0, 0.23)',
+					strokeWidth: 1,
+					cy: '10'
+				}}
+			/>
+			<Area
+				type="monotone"
+				dataKey="count"
+				stroke="rgba(183, 211, 255, 1)"
+				strokeWidth={2}
+				fillOpacity={1}
+				activeDot={{
+					stroke: 'rgba(0, 0, 0, 0.23)',
+					fill: 'rgba(255, 255, 255, 1)'
+				}}
+				fill="url(#colorUv)"
+			/>
+		</AreaChart>
+	</ResponsiveContainer>
 );
 
 TimeChart.propTypes = {
-  chartData: chartDataType.isRequired,
+	chartData: chartDataType.isRequired
 };
 
-export default withStyles(styles)(TimeChart);
+export default TimeChart;
